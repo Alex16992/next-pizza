884c58cf08a970e4002a100089f167beca18bbac
Alex_1699 <lehad600@gmail.com>
2025-04-23 14:42:25 +0500

    feat: Подключена база данных

---
+++ app/api/products/search/route.ts
@@ -0,0 +1,11 @@
+import { prisma } from "@/prisma/prisma-client";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+
+    const query = request.nextUrl.searchParams.get('query') || '';
+
+    const products = await prisma.product.findMany({ where: { name: { contains: query, mode: 'insensitive' } } });
+
+    return NextResponse.json(products);
+}
